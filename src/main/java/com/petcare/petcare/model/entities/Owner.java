2a9e8079cfa83be3a0138f1d72e71bcb90f2d099
Jullianag <juh_g@hotmail.com>
2025-10-24 15:02:20 -0300

    UserDetails e GrantedAuthority, sem teste

--- src/main/java/com/petcare/petcare/model/entities/Owner.java
+++ src/main/java/com/petcare/petcare/model/entities/Owner.java
@@ -1,13 +1,15 @@
 package com.petcare.petcare.model.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
 import java.util.*;
 
 @Entity
 @Table(name = "tb_owner")
-public class Owner {
+public class Owner implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -108,6 +110,36 @@ public class Owner {
             }
         }
         return false;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     @Override
