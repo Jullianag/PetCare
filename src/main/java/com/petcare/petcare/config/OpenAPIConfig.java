6e23b48da9b88520f855b1c473c32274ee933605
Jullianag <juh_g@hotmail.com>
2025-12-04 12:52:01 -0300

    OpenAPIConfig: classe de configuracao

---
+++ src/main/java/com/petcare/petcare/config/OpenAPIConfig.java
@@ -0,0 +1,25 @@
+package com.petcare.petcare.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI petcareAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Petcare API")
+                        .description("PetCare Reference Project")
+                        .version("v0.0.1")
+                        .license(new License()
+                                .name("MIT License")
+                                .url("https://opensource.org/licenses/MIT")));
+    }
+}
